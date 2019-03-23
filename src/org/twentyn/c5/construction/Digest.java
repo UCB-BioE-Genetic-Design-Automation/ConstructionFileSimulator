9b58c16bca44c018b8aac447174c1325bdd6735d
J. Christopher Anderson <chris@20n.com>
2019-03-23 08:18:44 -0700

    Digest working on BamHI example with 2 sites

--- src/org/twentyn/c5/construction/Digest.java
+++ src/org/twentyn/c5/construction/Digest.java
@@ -10,99 +10,129 @@ import org.twentyn.c5.construction.model.Polynucleotide;
 import org.twentyn.c5.utils.SequenceUtils;
 
 /**
- * 
- * A Function that inputs a DNA sequence and a List of restriction enzymes
- * and computes all the fragments that will be present upon digestion
- * to completion
- * 
+ *
+ * A Function that inputs a DNA sequence and a List of restriction enzymes and
+ * computes all the fragments that will be present upon digestion to completion
+ *
  * @author J. Christopher Anderson
  */
 public class Digest {
 
+    public void initiate() throws Exception {
+
+    }
+
     public List<Polynucleotide> run(Polynucleotide substrate, List<RestrictionEnzyme> enzymes) throws Exception {
         List<Polynucleotide> out = new ArrayList<>();
-        
+        List<Polynucleotide> temp = new ArrayList<>();
         out.add(substrate);
-        
-        //Iteratively cut the substrate with each enzyme
-        for(RestrictionEnzyme enz : enzymes) {
-            //Move all fragments into temp
-            List<Polynucleotide> temp = new ArrayList<>();
+
+        outer: while (true) {
+            boolean isdone = true;
+            temp.clear();
             temp.addAll(out);
             out.clear();
-            
-            //Scan through the first strand, breaking into fragments
-            for(Polynucleotide poly : temp) {
-                List<Polynucleotide> frags = cutOneSide(poly, enz);
-                for(Polynucleotide frag : frags) {
-                    Polynucleotide rc = revcomp(frag);
-                    List<Polynucleotide> rcfrags = cutOneSide(rc, enz);
-                    out.addAll(rcfrags);
+
+            inner: for (Polynucleotide poly : temp) {
+                List<Polynucleotide> result = cutOnce(poly, enzymes);
+                if (result == null) {
+                    Polynucleotide rc = revcomp(poly);
+                    result = cutOnce(rc, enzymes);
                 }
+                if (result == null) {
+                    out.add(poly);
+                    continue inner;
+                }
+
+                out.addAll(result);
+                isdone = false;
+            }
+            
+            if(isdone == true) {
+                break outer;
             }
         }
         return out;
     }
-    
+
     /**
-     * Exhaustively cut one polynucleotide with one restriction enzyme, and 
-     * return the fragments as a list including their sticky ends
+     * Helper method.  Finds the first of the enzymes provided as arguments, to
+     * find the first site it encounters.  The DNA is split into two fragments,
+     * which are returned in a List along with their sticky ends.
+     * 
      * @param sub
-     * @param enz
+     * @param enzymes
      * @return 
      */
-    private List<Polynucleotide> cutOneSide(Polynucleotide sub, RestrictionEnzyme data) {
+    private List<Polynucleotide> cutOnce(Polynucleotide sub, List<RestrictionEnzyme> enzymes) {
         List<Polynucleotide> out = new ArrayList<>();
-        Pattern p = Pattern.compile(data.getSite());
-        Matcher m = p.matcher(sub.getSequence().toUpperCase());
-        
-        Polynucleotide remaining = new Polynucleotide(sub.getSequence(), sub.getExt5(), sub.getExt3());
-        
-        while(m.find()) {
-            int start = m.start();
-            int end = m.end();
-            
-            //Construct the 3' remaining fragment
-            String remainingSequence = sub.getSequence().substring(m.start() + data.getCut3());
-            String remainingExt5 = sub.getSequence().substring(start + data.getCut5(), start + data.getCut3());
-            String newfrag = sub.getSequence().substring(0, start + data.getCut5());
-            String fragExt3 = sub.getSequence().substring(start + data.getCut5(), start + data.getCut3());
-            
-            Polynucleotide frag = new Polynucleotide(newfrag, sub.getExt5(), fragExt3);
-            out.add(frag);
+
+        for (RestrictionEnzyme enz : enzymes) {
+            Pattern p = Pattern.compile(enz.getSite());
+            Matcher m = p.matcher(sub.getSequence().toUpperCase());
+            while (m.find()) {
+                int start = m.start();
+                int end = m.end();
+                
+                //Construct the 5' remaining fragment
+                {
+                String remaining = sub.getSequence().substring(0, start + enz.getCut5());
+                String ext5 = sub.getExt5();
+                String ext3 = sub.getSequence().substring(start + enz.getCut5(), start + enz.getCut3());
+
+                Polynucleotide frag = new Polynucleotide(remaining, ext5, ext3);
+                out.add(frag);
+                }
+                
+                //Construct the 3' remaining fragment
+                {
+                String remaining = sub.getSequence().substring(start + enz.getCut3());
+                String ext5 = sub.getSequence().substring(start + enz.getCut5(), start + enz.getCut3());
+                String ext3 = sub.getExt3();
+
+                Polynucleotide frag = new Polynucleotide(remaining, ext5, ext3);
+                out.add(frag);
+                }
+                
+                return out;
+            }
         }
-        out.add(remaining);
-        return out;
+        
+        //Returns null if nothing is found
+        return null;
     }
 
     private Polynucleotide revcomp(Polynucleotide frag) {
         String rc = SequenceUtils.reverseComplement(frag.getSequence());
-        
+
         //Transfer the 3' end
         String outext3 = SequenceUtils.reverseComplement(frag.getExt5());
         String outext5 = SequenceUtils.reverseComplement(frag.getExt3());
-        
+
         Polynucleotide out = new Polynucleotide(rc, outext5, outext3);
         return out;
     }
-    
+
     public static void main(String[] args) throws Exception {
         List<RestrictionEnzyme> enz = new ArrayList<>();
-        Polynucleotide poly = new Polynucleotide("aaaaGGTCTCtCCCCttCAATTGccccc");
-        
+//        Polynucleotide poly = new Polynucleotide("aaaaGGTCTCtCCCCttCAATTGccccc");
+        Polynucleotide poly = new Polynucleotide("aaaGGATCCtttGGATCCcccaaa", "", "ggggg");
+
+
         RestrictionEnzymeFactory factory = new RestrictionEnzymeFactory();
         factory.initiate();
-        
-        enz.add(factory.run(Enzyme.BsaI));
-        
+
+        enz.add(factory.run(Enzyme.BamHI));
+
         Digest dig = new Digest();
         List<Polynucleotide> pdts = dig.run(poly, enz);
-       
-        for(Polynucleotide pol : pdts) {
+
+        for (Polynucleotide pol : pdts) {
             System.out.println(pol.toString());
         }
-        
+
         System.out.println("number of products: " + pdts.size());
-        
+
     }
+
 }
