ae10c5cd856cd697b798d6dad30b6a108b5dc5d6
J. Christopher Anderson <jcanderson@berkeley.edu>
2020-09-10 19:21:52 -0700

    Minor corrections and formatting

--- src/org/ucb/c5/constructionfile/simulators/ExtensionSimulator.java
+++ src/org/ucb/c5/constructionfile/simulators/ExtensionSimulator.java
@@ -49,7 +49,9 @@ public class ExtensionSimulator {
      *
      * Returns -1 if there are no annealing site with Tm >40degC
      *
-     * Throws an Exception if more than one annealing site >55degC is present
+     * Returns -2 if more than one annealing site >55degC is present
+     *
+     * Throws an Exception if an error is encountered
      *
      * Otherwise returns the highest-Tm site
      *
@@ -68,65 +70,66 @@ public class ExtensionSimulator {
         double bestscore = -1.0;
         String rcB = rc.run(template);
         while (true) {
-            
+
             //Find the next 6bp match, quit if there are no more
             index = rcB.indexOf(sixbp, index + 1);
             if (index == -1) {
                 break;
             }
 
-            //replace the last 6 bases with X           
+            //replace the last 6 bases with U          
             String oligoX = oligo.substring(0, oligo.length() - 6) + "UUUUUU";
             String rcX = rcB.substring(0, index) + "UUUUUU";
-                
+
             //align the sequences with Jaligner and print out the score  	
-			Sequence s1 = new Sequence(oligoX.toUpperCase());  
-			Sequence s2 = new Sequence(rcX.toUpperCase());
-	        
-	        Alignment alignment = SmithWatermanGotoh.align(s1, s2, MatrixLoader.load("EDNAFULL_1"), 10f, 0.5f);
-                double score = alignment.getScore();
-                if(score > bestscore){
-                    bestscore = score;
-                    bestIndex = index;
-                }
-	        System.out.println ( alignment.getSummary() );
-	        System.out.println ( new Pair().format(alignment) );
-	        	       
+            Sequence s1 = new Sequence(oligoX.toUpperCase());
+            Sequence s2 = new Sequence(rcX.toUpperCase());
+
+            //Use a modified EDNAFULL matrix to align and force the U's to align
+            Alignment alignment = SmithWatermanGotoh.align(s1, s2, MatrixLoader.load("EDNAFULL_1"), 10f, 0.5f);
+            double score = alignment.getScore();
+            if (score > bestscore) {
+                bestscore = score;
+                bestIndex = index;
+            }
+            System.out.println(alignment.getSummary());
+            System.out.println(new Pair().format(alignment));
+
             System.out.println(oligo);
             System.out.println(oligoX);
             System.out.println(rcB);
             System.out.println(rcX);
         }  //end while
-        
-        System.out.println("bestscore"+ bestscore);
-        System.out.println("bestIndex"+ bestIndex);
+
+        System.out.println("bestscore" + bestscore);
+        System.out.println("bestIndex" + bestIndex);
         //the two temperature constraints
 
         //Return the position on the template for the 3' end of the oligo
-        return bestIndex;
+        return bestIndex + 6;
     }
 
     public static void main(String[] args) throws Exception {
         ExtensionSimulator sim = new ExtensionSimulator();
         sim.initiate();
         /*
-        {
-            System.out.println("Correct duplex of 22 mers");
-            int result = sim.run("GTATCACGAGGCAGAATTTCAG", "CTGAAATTCTGCCTCGTGATAC");
-            System.out.println(result);
-        }
-        {
-            System.out.println("Mismatched 22 mers");
-            int result = sim.run("CTCCTTCGGTCCTCCGATCGTT", "CTGAAATTCTGCCTCGTGATAC");
-            System.out.println(result);
-        }
-        {
-            System.out.println("Mismatched 22 mers");
-            int result = sim.run("GGATCCCGTGGCAGTATTTAAG", "CTGAAATTCTGCCTCGTGATAC");
-            System.out.println(result);
+         {
+         System.out.println("Correct duplex of 22 mers");
+         int result = sim.run("GTATCACGAGGCAGAATTTCAG", "CTGAAATTCTGCCTCGTGATAC");
+         System.out.println(result);
+         }
+         {
+         System.out.println("Mismatched 22 mers");
+         int result = sim.run("CTCCTTCGGTCCTCCGATCGTT", "CTGAAATTCTGCCTCGTGATAC");
+         System.out.println(result);
+         }
+         {
+         System.out.println("Mismatched 22 mers");
+         int result = sim.run("GGATCCCGTGGCAGTATTTAAG", "CTGAAATTCTGCCTCGTGATAC");
+         System.out.println(result);
         
          */
-        System.out.println("Correct duplex of 22 mers");      
+        System.out.println("Correct duplex of 22 mers");
         int result = sim.run("cgtatGAATTCattaccgcctttgagtgagc".toUpperCase(), "TATTTTGACTGATAGTGACCTGTTCGTTGCAACAAATTGATGAGCAATGCTTTTTTATAATGCCAACTTTGTACAAAAAAGCAGGCTCCGAATTGgtatcacgaggcagaatttcagataaaaaaaatccttagctttcgctaaggatgatttctgGAATTCATGAGATCTTCCCTATCAGTGATAGAGATTGACATCCCTATCAGTGATAGAGATACTGAGCACGGATCTGAAAGAGGAGAAAGGATCTATGGCAAGTAGCGAAGACGTTATCAAAGAGTTCATGCGTTTCAAAGTTCGTATGGAAGGTTCCGTTAACGGTCACGAGTTCGAAATCGAAGGTGAAGGTGAAGGTCGTCCGTACGAAGGTACCCAGACCGCTAAACTGAAAGTTACCAAAGGTGGTCCGCTGCCGTTCGCTTGGGACATCCTGTCCCCGCAGTTCCAGTACGGTTCCAAAGCTTACGTTAAACACCCGGCTGACATCCCGGACTACCTGAAACTGTCCTTCCCGGAAGGTTTCAAATGGGAACGTGTTATGAACTTCGAAGACGGTGGTGTTGTTACCGTTACCCAGGACTCCTCCCTGCAAGACGGTGAGTTCATCTACAAAGTTAAACTGCGTGGTACCAACTTCCCGTCCGACGGTCCGGTTATGCAGAAAAAAACCATGGGTTGGGAAGCTTCCACCGAACGTATGTACCCGGAAGACGGTGCTCTGAAAGGTGAAATCAAAATGCGTCTGAAACTGAAAGACGGTGGTCACTACGACGCTGAAGTTAAAACCACCTACATGGCTAAAAAACCGGTTCAGCTGCCGGGTGCTTACAAAACCGACATCAAACTGGACATCACCTCCCACAACGAAGACTACACCATCGTTGAACAGTACGAACGTGCTGAAGGTCGTCACTCCACCGGTGCTTAATAAGGATCTCCAGGCATCAAATAAAACGAAAGGCTCAGTCGAAAGACTGGGCCTTTCGTTTTATCTGTTGTTTGTCGGTGAACGCTCTCTACTAGAGTCACACTGGCTCACCTTCGGGTGGGCCTTTCTGCGTTTATAGGATCCtaaCTCGAcgtgcaggcttcctcgctcactgactcgctgcgctcggtcgttcggctgcggcgagcggtatcagctcactcaaaggcggtaatCAATTCGACCCAGCTTTCTTGTACAAAGTTGGCATTATAAAAAATAATTGCTCATCAATTTGTTGCAACGAACAGGTCACTATCAGTCAAAATAAAATCATTATTTG".toUpperCase());
         System.out.println(result);
     }
