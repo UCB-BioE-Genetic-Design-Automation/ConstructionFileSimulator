fa0d4d1e5e3e405e22ca7ccd37151162396187a4
J. Christopher Anderson <jcanderson@berkeley.edu>
2020-09-17 17:53:13 -0700

    Stub for ExtensionSimulatorTest

---
+++ test/org/ucb/c5/constructionfile/simulators/ExtensionSimulatorTest.java
@@ -0,0 +1,25 @@
+package org.ucb.c5.constructionfile.simulators;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author J. Christopher Anderson
+ */
+
+public class ExtensionSimulatorTest {
+    @Test
+    public void testPositiveAnneals() throws Exception {
+     
+    }
+    
+    @Test
+    public void testNegativeAnneals() throws Exception {
+     
+    }
+    
+    @Test
+    public void testExceptionCases() throws Exception {
+     
+    }
+}
