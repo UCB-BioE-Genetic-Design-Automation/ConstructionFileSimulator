9f8a26d5d95de51ee7f3832d82c7d461b06adda8
J. Christopher Anderson <jcanderson@berkeley.edu>
2021-04-15 13:34:53 -0700

    Changed GUI to have button for choosing default features

--- src/org/ucb/c5/constructionfile/view/FileDropGUI.java
+++ src/org/ucb/c5/constructionfile/view/FileDropGUI.java
@@ -9,6 +9,10 @@ package org.ucb.c5.constructionfile.view;
  * BorderLayoutDemo.java
  *
  */
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.*;
 import javax.swing.border.Border;
 import org.ucb.c5.constructionfile.SimulateExperimentDirectory;
@@ -21,14 +25,13 @@ public class FileDropGUI extends JFrame {
     Border greenborder = javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(0, 153, 51), new java.awt.Color(102, 255, 102), new java.awt.Color(0, 102, 0), new java.awt.Color(204, 255, 204));
     Border whiteborder = javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white, java.awt.Color.white);
     JLabel imgLabel;
+    JButton featureBtn;
 
     public FileDropGUI() {
         super("ConstructionFileSimulator");
         initComponents();
         imgLabel.setBorder(whiteborder);
 
-
-        
         //Pack and show
         pack();
         setVisible(true);
@@ -41,8 +44,9 @@ public class FileDropGUI extends JFrame {
     private void initComponents() {
         //Create and set up the window.
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
 
-        //Add content
+        //Add background image
         imgLabel = new JLabel();
         imgLabel.setBackground(new java.awt.Color(255, 255, 255));
         imgLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/ucb/c5/constructionfile/view/filedropimg.png"))); // NOI18N
@@ -52,9 +56,18 @@ public class FileDropGUI extends JFrame {
                 mouseEnterHandler(evt);
             }
         });
-        getContentPane().add(imgLabel);
-        
-        
+        getContentPane().add(imgLabel, BorderLayout.CENTER);
+
+        //Add buttons
+        featureBtn = new JButton("Set feature file");
+        featureBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setFeatureFile();
+            }
+        });
+        getContentPane().add(featureBtn, BorderLayout.SOUTH);
+
         new FileDrop(System.out, this.getContentPane(), new FileDrop.Listener() {
             @Override
             public void filesDropped(java.io.File[] files) {
@@ -62,19 +75,6 @@ public class FileDropGUI extends JFrame {
                 for (int i = 0; i < files.length; i++) {
                     try {
                         String dirPath = files[i].getCanonicalPath();
-                        if (!files[i].isDirectory()) {
-                            if (dirPath.endsWith(".txt")) {
-                                String data = "default_features\t" + dirPath + "\n";
-                                FileUtils.writeFile(data, "data_paths.txt");
-                                Log.info("default_features set to " + dirPath);
-                                imgLabel.setBorder(greenborder);
-                                return;
-                            }
-
-                            //Otherwise it is some other kind of file, so abort
-                            imgLabel.setBorder(redborder);
-                            return;
-                        }
 
                         //Simulate the experiment and write results
                         SimulateExperimentDirectory sed = new SimulateExperimentDirectory();
@@ -93,6 +93,32 @@ public class FileDropGUI extends JFrame {
 
     private void mouseEnterHandler(java.awt.event.MouseEvent evt) {
         imgLabel.setBorder(whiteborder);
+    }
+
+    private void setFeatureFile() {
+        System.out.println("setFeatureFile");
+
+        JFileChooser fc = new JFileChooser();
+        fc.showOpenDialog(this);
+
+        try {
+            File file = fc.getSelectedFile();
+            String dirPath = file.getAbsolutePath();
+            if (dirPath.endsWith(".txt")) {
+                String data = "default_features\t" + dirPath + "\n";
+                FileUtils.writeFile(data, "data_paths.txt");
+                Log.info("default_features set to " + dirPath);
+                featureBtn.setBorder(greenborder);
+                Log.info("default_features set to: " + dirPath);
+                return;
+            } else {
+                featureBtn.setBorder(redborder);
+                Log.warning("Change of default_features failed, wrong file type provided");
+            }
+        } catch (Exception err) {
+            featureBtn.setBorder(redborder);
+            Log.warning("Failed to change default_features");
+        }
     }
 
     public static void main(String[] args) {
