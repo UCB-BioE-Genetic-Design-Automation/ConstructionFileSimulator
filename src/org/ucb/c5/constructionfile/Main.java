dd9d3596af88d44730b49cc31ff7dddd66a88739
J. Christopher Anderson <jcanderson@berkeley.edu>
2023-06-12 11:21:21 -0700

    Updated Main

---
+++ src/org/ucb/c5/constructionfile/Main.java
@@ -0,0 +1,33 @@
+package org.ucb.c5.constructionfile;
+
+import org.ucb.c5.constructionfile.model.Experiment;
+import org.ucb.c5.constructionfile.view.FileDropGUI;
+
+/**
+ *
+ * @author J. Christopher Anderson
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            //creating and showing this application's GUI.
+            javax.swing.SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    new FileDropGUI();
+                }
+            });
+        } else {
+            String dirPath = args[0];
+            ParseExperimentDirectory parseFolder = new ParseExperimentDirectory();
+            parseFolder.initiate();
+            Experiment exp = parseFolder.run(dirPath);
+
+            //Simulate the experiment and write results
+            SimulateExperimentDirectory sed = new SimulateExperimentDirectory();
+            sed.initiate();
+            Experiment simulatedExperiment = sed.run(exp);
+        }
+    }
+}
